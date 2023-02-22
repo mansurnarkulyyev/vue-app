277d8f4061530089750562f2b75d2a63837d8054
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2023-02-22 22:10:59 +0200

    created Product page

---
+++ src/components/product/ProductsOwner.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="products-owner">
+        <h2 class="products-owner__title">Информация о владельце:</h2>
+        <p class="products-owner__info">{{ owner.name }}</p>
+        <p class="products-owner__info">tel: {{ owner.phone }}</p>
+        <p class="products-owner__info">email: {{ owner.email }}</p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ProductsOwner',
+    props: {
+        owner: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+.products-owner {
+    padding: 20px;
+    background: $card-bg;
+    font-weight: 400;
+
+    &__title {
+        font-size: 18px;
+        font-weight: 700;
+        margin-bottom: 20px;
+    }
+
+    &__info {
+        margin-top: 6px;
+    }
+}</style>
--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -1,37 +1,63 @@
 <template>
-    <div>
+     <main class="product-page">
         <Container>
-           <ProductsMainInfo :product="product"/>
+             <div  class="product-page__content">
+                <ProductsMainInfo :product="product"/>
+                <div class="apartment-page__additional-info">
+                <ProductsOwner 
+                    class="product-page__owner"
+                     :owner="product.owner"/>
+                      <ProductReviews :reviews="reviewsList" />
+                </div>
+
+            </div>
         </Container>
-    </div>
+     </main>
 </template>
 
 <script>
 import Container from '../components/shared/Container.vue'
 import products from '../components/product/products'
 import ProductsMainInfo from '../components/product/ProductsMainInfo.vue'
+import ProductsOwner from '../components/product/ProductsOwner.vue'
+import ProductReviews from '../components/reviews';
+import reviewsList from '../components/reviews/reviews.json';
 
     export default {
         name:'ProductPage',
         components:{
             Container,
             ProductsMainInfo,
+            ProductsOwner,
+            ProductReviews,
         },
         computed:{
+            reviewsList(){
+                return reviewsList;
+            },
             product(){
                 return products.find(
                     (product) => product.id === this.$route.params.id
                 )
             }
         },
-        mounted(){
-            console.log(this.product);
-            console.log(this.route);
-        }
+      
         
     }
 </script>
 
 <style lang="scss" scoped>
-
+.product-page {
+  padding-bottom: 55px;
+  &__content {
+    display: flex;
+    align-items: flex-start;
+  }
+  &__additional-info {
+    margin-left: 30px;
+    max-width: 350px;
+    flex-grow: 0;
+    flex-shrink: 1;
+  }
+}
 </style>
